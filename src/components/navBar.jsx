e3f5a83dce9b48a04ce8bd37d31693d437856f90
Chris Daming <chris@legalgps.com>
2021-08-24 16:11:28 -0500

    updated FSDI 109

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react"; //imrc
+import storeContext from "../store/storeContext";
 import "./navBar.css"; //you have to import your CSS
 import { Link } from "react-router-dom";
 
@@ -6,14 +7,15 @@ import { Link } from "react-router-dom";
 
 // cc
 class NavBar extends Component {
+  static contextType = storeContext;
   state = {};
   render() {
     return (
-      <nav className="navbar navbar-dark bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/#">
+          <Link className="navbar-brand" to="/#">
             Legal GPS | For Schools
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -28,72 +30,31 @@ class NavBar extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="/cart"
-                >
-                  cart
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
                 </Link>
               </li>
+
               <li className="nav-item">
-                <a className="nav-link" href="/#">
-                  Link
-                </a>
+                <Link className="nav-link" to="/catalog">
+                  Catalog
+                </Link>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider"></hr>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
+
               <li className="nav-item">
-                <a
-                  className="nav-link disabled"
-                  href="/#"
-                  tabIndex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
               </li>
             </ul>
             <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
+              <Link className="btn-btn-outline-primary" to="/cart">
+                <i className="fa fa-cart-plus mr-1"></i>
+                View Cart &nbsp;&nbsp;&nbsp;
+                <span className="badge badge-dark ml-1">
+                  {this.context.cart.length}
+                </span>
+              </Link>
             </form>
           </div>
         </div>
